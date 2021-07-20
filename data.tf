eb7902fb80c522b74b7f4d4e1e1d22a12a29fc4b
Nitin-stack <nitin7.tewatia@gmail.com>
2021-07-20 13:56:03 +0530

    data files key vault and tags in terraform without errros

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "azurerm_key_vault" "key-vault" {
+data "azurerm_key_vault" "key_vault" {
     name = "lerntf-vault"
     resource_group_name = "remote-state"
 }
@@ -6,5 +6,5 @@ data "azurerm_key_vault" "key-vault" {
 data "azurerm_key_vault_secret" "admin_password" {
   
   name = "admin-password"
-  key_vault_id = azurerm_key_vault.key-vault.id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
